4a7b401a1ce49b41dc33801412387932be108aac
Davide <davidemadonna@outlook.it>
2021-01-05 19:32:10 +0100

    Added 'weather' command, atm is just an echo call to see if everything works as intended

--- Install.sh
+++ Install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #path="PATH=\$PATH:"$(pwd)
 #echo -e '\n# Path to OpenWeather bash command\n'$path >> $HOME/.bashrc
-echo -e '\n# Path to OpenWeather bash command\nPATH=$PATH:'$(pwd) >> $HOME/.bashrc
+echo -e '\n# Path to OpenWeather bash command\nexport PATH=$PATH:'$(pwd) >> $HOME/.bashrc
