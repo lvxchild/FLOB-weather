95d34f2f459250f606408321fb2f670dda437d51
Davide <davidemadonna@outlook.it>
2021-01-09 15:02:24 +0100

    Cleaned code in Uninstall.sh

--- Uninstall.sh
+++ Uninstall.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-# Create correct pwd formula for use with sed #
-route=$(pwd) 
-route="$(echo $route | sed 's/\//\\\//g')"
+#
+echo "---OPEN WEATHER UNINSTALLER---"
+echo "You are removing Open Weather CLI, are you sure(y/n)?"
+read isSure
 
-# Remove PATH from .bashrc #
-sed -i '/Path to OpenWeather bash commands/d' $HOME/.bashrc
-sed -i '/export PATH=$PATH:'$route'/d' $HOME/.bashrc
+# If user really want to delete: ask if user also want to delete the Open Weather CLI folder, create correct pwd formula for use with sed and finally remove PATH from /bashrc and folder # 
+if [[ $isSure == "y" ]]
+then
+	echo "Would you like to also delete this folder and everything in it(y/n)?"
+	read deleteFolders
+	route="$(echo $(pwd) | sed 's/\//\\\//g')"
+	sed -i '/Path to OpenWeather bash commands/d' $HOME/.bashrc
+	sed -i '/export PATH=$PATH:'$route'/d' $HOME/.bashrc
+	if [[ $deleteFolders == "y" ]]
+	then
+		rm -r $(pwd)
+	fi
+fi
